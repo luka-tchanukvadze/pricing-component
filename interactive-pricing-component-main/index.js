89ec324d00fa823b1d8e0d2e6cb1bd0e71ec3b2b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2023-07-29 15:29:42 +0400

    simpler js

--- interactive-pricing-component-main/index.js
+++ interactive-pricing-component-main/index.js
@@ -1,51 +1,23 @@
-function getElementByClass(elementClass){
-  return document.querySelector(`.${elementClass}`)
 
+const views = document.querySelector('.views')
+const price = document.querySelector('.number')
+const range = document.querySelector('.myRange')
+const checkBox = document.querySelector('.check-box')
+
+const prices = [8, 12, 16, 24, 36]
+const pageViews = ['10K', '50K', '100K', '500K', '1M']
+
+function updateViewsAndPrice() {
+  const index = range.value - 1
+  const discountedPrice = prices[index] * 0.75
+  const selectedViews = pageViews[index]
+  const monthlyPrice = checkBox.checked ? discountedPrice : prices[index]
+
+  views.textContent = `${selectedViews} PAGEVIEWS`
+  price.textContent = `$${monthlyPrice.toFixed(2)}`
 }
 
+range.addEventListener('input', updateViewsAndPrice)
+checkBox.addEventListener('change', updateViewsAndPrice)
 
-const views = getElementByClass('views')
-const price = getElementByClass('number')
-const range = getElementByClass('myRange')
-
-function income(){
-  let myValue = Number(range.value)
-  let number = 0
-  let inMonth = 0
-  
-  switch(true){
-    case myValue === 1:
-      number = 10
-      inMonth = 8
-      break
-    case myValue === 2:
-      number = 50
-      inMonth = 12
-      break
-    case myValue === 3:
-      number = 100
-      inMonth = 16
-      break
-    case myValue === 4:
-      number = 500
-      inMonth = 24
-      break
-    case myValue === 5:
-      number = 1
-      inMonth = 36
-      break
-  }
-
-  if(myValue < 5){
-    views.textContent = `${number}k pageviews`.toUpperCase()
-    price.textContent = `$${inMonth.toFixed(2)}`
-  } else if(myValue === 5){
-    views.textContent = `${number}m pageviews`.toUpperCase()
-    price.textContent = `$${inMonth.toFixed(2)}`
-  }
-
-}
-
-range.addEventListener('input', () => {
-  income()
-})
+updateViewsAndPrice()
